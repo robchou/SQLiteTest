ffb8971d438314488547d1f67ea1aad6ea33bd4a
Robin <robin.m.chou@gmail.com>
2016-03-25 17:47:23 +0800

    Add writeStringToFile and  getTempFile

--- app/src/main/java/com/androidhive/sqlitetest/MainActivity.java
+++ app/src/main/java/com/androidhive/sqlitetest/MainActivity.java
@@ -1,12 +1,15 @@
 package com.androidhive.sqlitetest;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -28,5 +31,37 @@ public class MainActivity extends AppCompatActivity {
         File dbFile = new File(context.getFilesDir(), DATABASE_NAME);
         dataBaseHelper.writeStringToFile(dbFile.getAbsolutePath(), "Hello, Duomai");
         Log.e(TAG, dbFile.getAbsolutePath());
+
+        writeStringToFile("hello", "Hello World!");
+        getTempFile(context, "hello");
+    }
+
+    //Write string to private file
+    public void writeStringToFile(String file, String content) {
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(file, Context.MODE_PRIVATE);
+            outputStream.write(content.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //Generate tmp file
+    public File getTempFile(Context context, String url) {
+        File file = null;
+
+        try {
+            String fileName = Uri.parse(url).getLastPathSegment();
+            file = File.createTempFile(fileName, null, context.getCacheDir());
+        } catch (IOException e) {
+            // Error while creating file
+            e.printStackTrace();
+        }
+
+        return file;
     }
 }
